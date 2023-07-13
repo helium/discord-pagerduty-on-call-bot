9030d68e27d15f33bf127029790715f8684c989f
Darwin Rinderer <darwin@helium.foundation>
2023-07-13 15:55:00 -0400

    Update handler

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { Client } = require('discord.js');
-const client = new Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-  console.log(`Logged in...`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
