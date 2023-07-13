4bae67718acfaa7e2eb0d0385c72288312536210
Darwin Rinderer <darwin@helium.foundation>
2023-07-13 15:19:35 -0400

    Basic shell

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Client } from 'discord.js';
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+client.on('message', msg => {
+  if (msg.content.indexOf("ping") === 0) { 
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
