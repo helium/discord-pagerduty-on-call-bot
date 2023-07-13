f2252f066555c513c080503df1c33a5196b75336
Darwin Rinderer <darwin@helium.foundation>
2023-07-13 17:20:43 -0400

    Revising

--- commands/utility/foundationPagerdutyOncall.js
+++ commands/utility/foundationPagerdutyOncall.js
@@ -5,6 +5,13 @@ module.exports = {
 		.setName('foundation')
 		.setDescription('Display active Foundation On-Call team members.'),
 	async execute(interaction) {
+    const res = await fetch('https://api.pagerduty.com/oncalls', {
+      Accept: 'application/vnd.pagerduty+json;version=2',
+      Authorization: `Token token=${process.env.FOUNDATION_PAGERDUTY_API_KEY}`,
+      'Content-Type': 'application/json'
+    });
+    const resJson = await res.json();
+    console.log(resJson);
 		return interaction.reply(`testing`);
 	},
 };
