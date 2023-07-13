7e915d912967e3fc98fd31d266bf4d0a02bf3906
Darwin Rinderer <darwin@helium.foundation>
2023-07-13 17:31:41 -0400

    Revising

--- commands/utility/foundationPagerdutyOncall.js
+++ commands/utility/foundationPagerdutyOncall.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { fetch } = require('node-fetch');
+const { fetch } = require('node-fetch-commonjs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
