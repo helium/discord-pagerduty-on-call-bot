b5b0967f3795561f637038718993ddb711e2686d
Darwin Rinderer <darwin@helium.foundation>
2023-07-13 17:12:53 -0400

    Revising

--- commands/utility/foundationPagerdutyOncall.js
+++ commands/utility/foundationPagerdutyOncall.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('FoundationOnCall')
+		.setName('Foundation')
 		.setDescription('Display active Foundation On-Call team members.'),
 	async execute(interaction) {
 		return interaction.reply(`testing`);
