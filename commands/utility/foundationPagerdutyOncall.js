0aa416e1f88a42df5cde8bbd68ccb9b7dc4ff5ff
Darwin Rinderer <darwin@helium.foundation>
2023-07-14 16:29:00 -0400

    Updating to use oauth token

--- commands/utility/foundationPagerdutyOncall.js
+++ commands/utility/foundationPagerdutyOncall.js
@@ -1,22 +1,38 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fetch = require('node-fetch');
 
-const { FOUNDATION_DISCORD_USER_MAP, FOUNDATION_PAGERDUTY_API_KEY } = process.env;
+const { FOUNDATION_DISCORD_USER_MAP, PAGERDUTY_CLIENT_ID, PAGERDUTY_CLIENT_SECRET } = process.env;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('foundation-oncall')
 		.setDescription('Display active Foundation On-Call team members.'),
 	async execute(interaction) {
-    const res = await fetch('https://api.pagerduty.com/oncalls', { 
+    const body = new URLSearchParams();
+    body.append('grant_type', 'client_credentials');
+    body.append('client_id', PAGERDUTY_CLIENT_ID);
+    body.append('client_secret', PAGERDUTY_CLIENT_SECRET);
+    body.append('scope', 'as_account-us.helium-foundation oncalls.read');
+
+    const oathRes = await fetch('https://identity.pagerduty.com/oauth/token', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: body.toString()
+    });
+
+    const { access_token } = await oathRes.json();
+
+    const oncallsRes = await fetch('https://api.pagerduty.com/oncalls', { 
       headers: {
         Accept: 'application/vnd.pagerduty+json;version=2',
-        Authorization: `Token token=${FOUNDATION_PAGERDUTY_API_KEY}`,
+        Authorization: `Bearer ${access_token}`,
         'Content-Type': 'application/json'
       }
     });
 
-    const json = await res.json();
+    const json = await oncallsRes.json();
     const userMap = JSON.parse(FOUNDATION_DISCORD_USER_MAP);
 
     let onCallUsers = '';
