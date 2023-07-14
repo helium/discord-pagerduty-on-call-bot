5c088f6030647247d5f9f656b68c12cc963c6609
Darwin Rinderer <darwin@helium.foundation>
2023-07-14 17:12:31 -0400

    Adding Nova

--- commands/utility/novaPagerdutyOncall.js
+++ commands/utility/novaPagerdutyOncall.js
@@ -1,18 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fetch = require('node-fetch');
 
-const { FOUNDATION_DISCORD_USER_MAP, FOUNDATION_PAGERDUTY_CLIENT_ID, FOUNDATION_PAGERDUTY_CLIENT_SECRET } = process.env;
+const { NOVA_PAGERDUTY_CLIENT_ID, NOVA_PAGERDUTY_CLIENT_SECRET } = process.env;
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('foundation-oncall')
-		.setDescription('Display active Foundation On-Call team members.'),
+		.setName('nova-oncall')
+		.setDescription('Display active Nova On-Call team members.'),
 	async execute(interaction) {
     const body = new URLSearchParams();
     body.append('grant_type', 'client_credentials');
-    body.append('client_id', FOUNDATION_PAGERDUTY_CLIENT_ID);
-    body.append('client_secret', FOUNDATION_PAGERDUTY_CLIENT_SECRET);
-    body.append('scope', 'as_account-us.helium-foundation oncalls.read');
+    body.append('client_id', NOVA_PAGERDUTY_CLIENT_ID);
+    body.append('client_secret', NOVA_PAGERDUTY_CLIENT_SECRET);
+    body.append('scope', 'as_account-us.helium-inc oncalls.read');
 
     const oathRes = await fetch('https://identity.pagerduty.com/oauth/token', { 
       method: 'POST',
@@ -33,13 +33,12 @@ module.exports = {
     });
 
     const json = await oncallsRes.json();
-    const userMap = JSON.parse(FOUNDATION_DISCORD_USER_MAP);
 
     let onCallUsers = '';
     json.oncalls.forEach((group) => {
-      if (group.escalation_level === 1) {
+      if (group.schedule.summary === 'EngOps primary') {
         const user = group.user.summary;
-        onCallUsers += `**${group.schedule.summary}**: ${user} (${userMap[user]})\n`
+        onCallUsers += `**${group.schedule.summary}**: ${user}\n`
       }
     });
 
