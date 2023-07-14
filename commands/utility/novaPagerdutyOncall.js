1fe75f19934789c5447b8e2f121d2c991d847ee7
Darwin Rinderer <darwin@helium.foundation>
2023-07-14 17:51:59 -0400

    Adding Nova

--- commands/utility/novaPagerdutyOncall.js
+++ commands/utility/novaPagerdutyOncall.js
@@ -40,7 +40,7 @@ module.exports = {
         const user = group.user.summary;
         onCallUsers += `**${group.schedule.summary}**: ${user}\n`
       }
-      if (group.escalation_policy && group.escalation_policy.summary === 'IoT Comms-ep') {
+      if (group.escalation_policy && group.escalation_policy.summary && group.escalation_policy.summary === 'IoT Comms-ep') {
         const user = group.user.summary;
         onCallUsers += `**${group.schedule.summary}**: ${user}\n`
       }
