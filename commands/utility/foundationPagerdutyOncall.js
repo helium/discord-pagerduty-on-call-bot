11ffec7734ae1ff91591bba04443cf0b40bf0f83
Darwin Rinderer <darwin@helium.foundation>
2023-07-14 12:18:48 -0400

    Revising

--- commands/utility/foundationPagerdutyOncall.js
+++ commands/utility/foundationPagerdutyOncall.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fetch = require('node-fetch');
 
+const { FOUNDATION_DISCORD_USER_MAP, FOUNDATION_PAGERDUTY_API_KEY } = process.env;
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('foundation')
@@ -9,19 +11,24 @@ module.exports = {
     const res = await fetch('https://api.pagerduty.com/oncalls', { 
       headers: {
         Accept: 'application/vnd.pagerduty+json;version=2',
-        Authorization: `Token token=${process.env.FOUNDATION_PAGERDUTY_API_KEY}`,
+        Authorization: `Token token=${FOUNDATION_PAGERDUTY_API_KEY}`,
         'Content-Type': 'application/json'
       }
     });
 
     const json = await res.json();
+    const userMap = JSON.parse(FOUNDATION_DISCORD_USER_MAP);
 
     let onCallUsers = '';
     json.oncalls.forEach((group) => {
       if (group.escalation_level === 1) {
-        onCallUsers += `${group.schedule.summary}: ${group.user.summary}\n`
+        const user = group.user.summary;
+        onCallUsers += `${group.schedule.summary}: ${user} (${userMap[user]})\n`
       }
-    })
+    });
+
+    console.log(userMap);
+    console.log(onCallUsers);
 
 		return interaction.reply(`${onCallUsers}`);
 	},
